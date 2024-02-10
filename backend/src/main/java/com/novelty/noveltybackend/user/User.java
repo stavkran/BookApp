8e4725d04bf5e830faaf763f1cffe5e6511f445b
GiorgosNik <giorgosnl17@gmail.com>
2024-02-10 18:00:45 +0200

    Create user class

---
+++ backend/src/main/java/com/novelty/noveltybackend/user/User.java
@@ -0,0 +1,42 @@
+package com.novelty.noveltybackend.user;
+
+public class User {
+    private Long id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ backend/src/main/java/com/novelty/noveltybackend/user/UserService.java
@@ -0,0 +1,17 @@
+package com.novelty.noveltybackend.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public List<User> getUsers(){
+        return List.of(
+                new User(
+                        1L,"Test"
+                )
+        );
+    }
+}
